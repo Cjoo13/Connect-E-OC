2f6cbcdfb527af06a22958edb49bedc7f6f19268
Cjoo13 <sejourneremy@gmail.com>
2021-10-29 23:49:17 +0200

    modif nom router link modifyaccount

--- Frontend/src/components/UserMenu.vue
+++ Frontend/src/components/UserMenu.vue
@@ -1,8 +1,7 @@
 <template>
         <nav id="user_menu">
             <ul>
-              <li><router-link to="/modify-account" title="Modifier votre compte">Modifier votre compte</router-link></li>
-              <li><router-link to="/delete-account" title="Supprimer votre compte">Supprimer votre compte</router-link></li>
+              <li><router-link to="/modify_account" title="Modifier votre compte">Modifier votre compte</router-link></li>
               <li><router-link to="/" title="Se déconnecter" @click="deconnect">Se déconnecter</router-link></li>
             </ul>
         </nav>
